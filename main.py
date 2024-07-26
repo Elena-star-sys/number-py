26270759aea232427228b391ea28ce76ef50cb64
Elena-star-sys <elenashulgaelena@gmail.com>
2024-07-26 06:32:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#homework1
+number = input("Введите число: ")
+print(int(number[0]) + int(number[-1]))
+
+number_1 = input("Введите первое число: ")
+number_2 = input("Введите второе число: ")
+print(int(len(number_1)) + int(len(number_2)))
+
+
+
